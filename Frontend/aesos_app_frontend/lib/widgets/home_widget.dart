00861158e0ce6f62e36c47474499e70594b1f508
Caros04 <jowk04@gmail.com>
2023-03-01 02:25:00 +0700

    add image but still eror when using svg picture

--- Frontend/aesos_app_frontend/lib/widgets/home_widget.dart
+++ Frontend/aesos_app_frontend/lib/widgets/home_widget.dart
@@ -1,12 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class MyWidget extends StatelessWidget {
+class HomeWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    //final screenSize = MediaQuery.of(context).size;
+    //final containerSize = screenSize.height * 0.2;
+    const svg = 'assets/chat_bubble.svg';
     return Container(
-      padding: EdgeInsets.all(16.0),
-      color: Colors.blue,
-      child: Text('Hello, World!', style: TextStyle(fontSize: 24.0)),
+      //width: screenSize.width * 100,
+      //height: containerSize,
+      //padding: EdgeInsets.all(16.0),
+      //color: Colors.blue,
+      child: ListView(children: [
+        SvgPicture.asset(svg, semanticsLabel: 'My SVG Picture'),
+      ]),
     );
   }
 }
