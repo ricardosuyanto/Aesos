968fcfbdb368fd7c14301c74cbb18afae2c96fdc
Caros04 <jowk04@gmail.com>
2023-03-14 22:01:12 +0700

    add onChange page

--- Frontend/aesos_app_frontend/lib/screens/login_screen.dart
+++ Frontend/aesos_app_frontend/lib/screens/login_screen.dart
@@ -1,11 +1,15 @@
 import 'dart:convert';
 
+import 'package:aesos_app_frontend/screens/home_screen.dart';
 import 'package:aesos_app_frontend/utils/color.dart';
 import 'package:aesos_app_frontend/widgets/text_field_input.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import '../model/user.dart';
 import '../resources/auth.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+import '../utils/utils.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -17,7 +21,8 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   final TextEditingController _usernameController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
-
+  final storage = FlutterSecureStorage();
+  bool _isLoading = false;
   @override
   void dispose() {
     super.dispose();
@@ -26,30 +31,33 @@ class _LoginScreenState extends State<LoginScreen> {
   }
 
   Future<void> login() async {
-    String username = _usernameController.text.trim();
-    String password = _passwordController.text.trim();
-
-    var res = await AuthMethod().login(username: username, password: password);
-
+    var res = await AuthMethod().login(
+        username: _usernameController.text, password: _passwordController.text);
+    setState(() {
+      _isLoading = true;
+    });
     if (res!.statusCode == 200) {
-      Map<String, dynamic> map = jsonDecode(res.body);
-
-      // String username = map['username'];
-      // String user = map['user'];
-      // String message = map['message'];
-      // print(message);
-      // print(user);
-      // print(username);
-
       final data = jsonDecode(res.body);
       // Map<String, dynamic> map = jsonDecode(res.body);
       // String name = map['name'];
       // int age = map['age'];
       Login login = Login.fromJson(data);
-      print(login.message);
+      await storage.write(key: 'token', value: login.token);
+
+      String? value = await storage.read(key: 'token');
+
+      print(value);
+
+      Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const HomeScreen()));
     } else {
       print("eror");
+
+      showSnackBar(res as BuildContext, context as String);
     }
+    setState(() {
+      _isLoading = true;
+    });
   }
 
   @override
@@ -103,7 +111,13 @@ class _LoginScreenState extends State<LoginScreen> {
                       ),
                     ),
                     color: blueColor),
-                child: const Text('Log in'),
+                child: _isLoading
+                    ? const Center(
+                        child: CircularProgressIndicator(
+                          color: primaryColor,
+                        ),
+                      )
+                    : Text('Log in'),
               ),
             ),
             const SizedBox(
