c6f051669b0a5265a2ce6fa0a7642749c0b757c8
unknown <julius.oscar@banksinarmas.com>
2023-03-16 14:29:43 +0700

    add post screen

---
+++ Frontend/aesos_app_frontend/lib/screens/add_post_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class AddPostScreen extends StatefulWidget {
+  const AddPostScreen({Key? key}) : super(key: key);
+
+  @override
+  _AddPostScreenState createState() => _AddPostScreenState();
+}
+
+class _AddPostScreenState extends State<AddPostScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
