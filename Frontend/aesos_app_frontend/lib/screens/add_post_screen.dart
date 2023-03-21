9ed86ffdf94b1b0e86dfd1ba97031c46e3cee6ce
Caros04 <jowk04@gmail.com>
2023-03-21 21:37:40 +0700

    make func get image base 64

--- Frontend/aesos_app_frontend/lib/screens/add_post_screen.dart
+++ Frontend/aesos_app_frontend/lib/screens/add_post_screen.dart
@@ -20,6 +20,7 @@ class AddPostScreen extends StatefulWidget {
 class _AddPostScreenState extends State<AddPostScreen> {
   File? _file;
   String? _base64Image;
+  final TextEditingController _descriptionController = TextEditingController();
 
   Future<void> uploadPost() async {
     print(_file);
@@ -37,7 +38,7 @@ class _AddPostScreenState extends State<AddPostScreen> {
     var res = await PostMethod().makePost(
         user_id: user.id,
         title: "testing",
-        description: "masuk",
+        description: _descriptionController.text,
         picture: _base64Image.toString());
 
     if (res!.statusCode == 200) {
@@ -45,6 +46,15 @@ class _AddPostScreenState extends State<AddPostScreen> {
     } else {
       print("eror ");
     }
+  }
+
+  Future<void> getImageBase64() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+    final bytes = await pickedFile!.readAsBytes();
+    setState(() {
+      _base64Image = base64.encode(bytes);
+    });
   }
 
   Future _selectImage(BuildContext context) async {
@@ -70,8 +80,14 @@ class _AddPostScreenState extends State<AddPostScreen> {
                 child: const Text('Choose a photo'),
                 onPressed: () async {
                   Navigator.of(context).pop();
-
-                  uploadPost();
+                  getImageBase64();
+                },
+              ),
+              SimpleDialogOption(
+                padding: const EdgeInsets.all(20),
+                child: const Text('Cancel'),
+                onPressed: () {
+                  Navigator.of(context).pop();
                 },
               )
             ],
@@ -113,46 +129,49 @@ class _AddPostScreenState extends State<AddPostScreen> {
                 )
               ],
             ),
-            body: Column(children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  CircleAvatar(
-                    backgroundImage: NetworkImage(
-                        'https://images.unsplash.com/photo-1679240017168-5d2adb0529a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'),
-                  ),
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.45,
-                    child: TextField(
-                      decoration: const InputDecoration(
-                        hintText: "Write a caption..",
-                        border: InputBorder.none,
+            body: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          'https://images.unsplash.com/photo-1679240017168-5d2adb0529a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'),
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width * 0.45,
+                      child: TextField(
+                        controller: _descriptionController,
+                        decoration: const InputDecoration(
+                          hintText: "Write a caption..",
+                          border: InputBorder.none,
+                        ),
+                        maxLines: 8,
                       ),
-                      maxLines: 8,
                     ),
-                  ),
-                  SizedBox(
-                    height: 45,
-                    width: 45,
-                    child: AspectRatio(
-                      aspectRatio: 487 / 451,
-                      child: Container(
-                          decoration: BoxDecoration(
-                              image: DecorationImage(
-                        image: _base64Image == null
-                            ? NetworkImage(
-                                'https://images.unsplash.com/photo-1679240017168-5d2adb0529a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80')
-                            : NetworkImage(base64Image),
-                        fit: BoxFit.fill,
-                        alignment: FractionalOffset.topCenter,
-                      ))),
+                    SizedBox(
+                      height: 45,
+                      width: 45,
+                      child: AspectRatio(
+                        aspectRatio: 487 / 451,
+                        child: Container(
+                            decoration: BoxDecoration(
+                                image: DecorationImage(
+                          image: _base64Image == null
+                              ? NetworkImage(
+                                  'https://images.unsplash.com/photo-1679240017168-5d2adb0529a7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80')
+                              : NetworkImage(base64Image),
+                          fit: BoxFit.fill,
+                          alignment: FractionalOffset.topCenter,
+                        ))),
+                      ),
                     ),
-                  ),
-                  const Divider(),
-                ],
-              )
-            ]),
+                    const Divider(),
+                  ],
+                )
+              ],
+            ),
           );
   }
 }
